bf3c60dae481e5c521b4bdfcae29eb3b4d2a5957
Stefano Colao <lordcoste@MacBook-Pro-di-Stefano.local>
2019-02-04 20:15:14 +0100

    resize on ios

--- src/ios/CTCrop.m
+++ src/ios/CTCrop.m
@@ -7,6 +7,8 @@
 @property (assign) NSUInteger quality;
 @property (assign) NSUInteger targetWidth;
 @property (assign) NSUInteger targetHeight;
+@property (assign) NSUInteger allowRotate;
+@property (assign) NSUInteger keepCropAspectRatio;
 @end
 
 @implementation CTCrop
@@ -15,17 +17,19 @@
     UIImage *image;
     NSString *imagePath = [command.arguments objectAtIndex:0];
     NSDictionary *options = [command.arguments objectAtIndex:1];
-    
+
     self.quality = options[@"quality"] ? [options[@"quality"] intValue] : 100;
     self.targetWidth = options[@"targetWidth"] ? [options[@"targetWidth"] intValue] : -1;
     self.targetHeight = options[@"targetHeight"] ? [options[@"targetHeight"] intValue] : -1;
+    self.allowRotate = options[@"allowRotate"] ? [options[@"allowRotate"] boolValue] : NO;
+    self.keepCropAspectRatio = options[@"keepCropAspectRatio"] ? [options[@"keepCropAspectRatio"] boolValue] : YES;
     NSString *filePrefix = @"file://";
-    
+
     if ([imagePath hasPrefix:filePrefix]) {
         imagePath = [imagePath substringFromIndex:[filePrefix length]];
     }
-    
-    
+
+
     if (!(image = [UIImage imageWithContentsOfFile:imagePath])) {
         NSDictionary *err = @{
                               @"message": @"Image doesn't exist",
@@ -35,30 +39,32 @@
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
         return;
     }
-    
+
     PECropViewController *cropController = [[PECropViewController alloc] init];
     cropController.delegate = self;
     cropController.image = image;
-    
+
     CGFloat width = self.targetWidth > -1 ? (CGFloat)self.targetWidth : image.size.width;
     CGFloat height = self.targetHeight > -1 ? (CGFloat)self.targetHeight : image.size.height;
     CGFloat length = MIN(width, height);
     cropController.toolbarHidden = YES;
-    cropController.rotationEnabled = NO;
-    cropController.keepingCropAspectRatio = YES;
-    
-    cropController.imageCropRect = CGRectMake((width - length) / 2,
-                                              (height - length) / 2,
-                                              length,
-                                              length);
-    
+    cropController.rotationEnabled = self.allowRotate;
+    cropController.keepingCropAspectRatio = self.keepCropAspectRatio;
+
+    if (self.keepCropAspectRatio) {
+        cropController.imageCropRect = CGRectMake((width - length) / 2,
+                                                  (height - length) / 2,
+                                                  length,
+                                                  length);
+    }
+
     self.callbackId = command.callbackId;
     UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:cropController];
-    
+
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
         navigationController.modalPresentationStyle = UIModalPresentationFormSheet;
     }
-    
+
     [self.viewController presentViewController:navigationController animated:YES completion:NULL];
 }
 
@@ -67,12 +73,13 @@
 - (void)cropViewController:(PECropViewController *)controller didFinishCroppingImage:(UIImage *)croppedImage {
     [controller dismissViewControllerAnimated:YES completion:nil];
     if (!self.callbackId) return;
-    
-    NSData *data = UIImageJPEGRepresentation(croppedImage, (CGFloat) self.quality);
+
+    UIImage *resizedImage = [self resizeImage:croppedImage];
+    NSData *data = UIImageJPEGRepresentation(resizedImage, (CGFloat) self.quality);
     NSString* filePath = [self tempFilePath:@"jpg"];
     CDVPluginResult *result;
     NSError *err;
-    
+
     // save file
     if (![data writeToFile:filePath options:NSAtomicWrite error:&err]) {
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION messageAsString:[err localizedDescription]];
@@ -80,7 +87,7 @@
     else {
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[[NSURL fileURLWithPath:filePath] absoluteString]];
     }
-    
+
     [self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
     self.callbackId = nil;
 }
@@ -98,18 +105,61 @@
 
 #pragma mark - Utilites
 
+-(UIImage *)resizeImage:(UIImage *)image
+{
+    float actualHeight = image.size.height;
+    float actualWidth = image.size.width;
+    float maxHeight = self.targetHeight;
+    float maxWidth = self.targetWidth;
+    float imgRatio = actualWidth/actualHeight;
+    float maxRatio = maxWidth/maxHeight;
+
+    if (actualHeight > maxHeight || actualWidth > maxWidth)
+    {
+        if(imgRatio < maxRatio)
+        {
+            //adjust width according to maxHeight
+            imgRatio = maxHeight / actualHeight;
+            actualWidth = imgRatio * actualWidth;
+            actualHeight = maxHeight;
+        }
+        else if(imgRatio > maxRatio)
+        {
+            //adjust height according to maxWidth
+            imgRatio = maxWidth / actualWidth;
+            actualHeight = imgRatio * actualHeight;
+            actualWidth = maxWidth;
+        }
+        else
+        {
+            actualHeight = maxHeight;
+            actualWidth = maxWidth;
+        }
+    }
+
+    CGRect rect = CGRectMake(0.0, 0.0, actualWidth, actualHeight);
+    UIGraphicsBeginImageContext(rect.size);
+    [image drawInRect:rect];
+    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+    NSData *imageData = UIImageJPEGRepresentation(img, 1.0);
+    UIGraphicsEndImageContext();
+
+    return [UIImage imageWithData:imageData];
+
+}
+
 - (NSString*)tempFilePath:(NSString*)extension
 {
     NSString* docsPath = [NSTemporaryDirectory()stringByStandardizingPath];
     NSFileManager* fileMgr = [[NSFileManager alloc] init]; // recommended by Apple (vs [NSFileManager defaultManager]) to be threadsafe
     NSString* filePath;
-    
+
     // generate unique file name
     int i = 1;
     do {
         filePath = [NSString stringWithFormat:@"%@/%@%03d.%@", docsPath, CDV_PHOTO_PREFIX, i++, extension];
     } while ([fileMgr fileExistsAtPath:filePath]);
-    
+
     return filePath;
 }
 
